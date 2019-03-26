dc8fe3cb64bd3a046d6579ae197ae893c529503d
Yu-hanCheng <sarahcheng1231@gmail.com>
2019-03-26 10:45:31 +0800

    ML usercode

--- gameserver/1/1/1/1_1.py
+++ gameserver/1/1/1/1_1.py
@@ -1,15 +1,14 @@
+import pickle
+import numpy as np
+filename="svm_model_YHPF_0325.sav"
+load_model = pickle.load(open(filename, 'rb'))
+
 def run():
     global paddle_vel,paddle_pos,ball_pos,move_unit
     paddle_vel=0
-    if (ball_pos[-1][1]-ball_pos[-2][1]) >0:
-        if ball_pos[-1][1]-paddle_pos<8:
-            paddle_vel=0
-        elif ball_pos[-1][1]-paddle_pos>8:
-            paddle_vel=move_unit*2
-    elif (ball_pos[-1][1]-ball_pos[-2][1])<0:
-        if ball_pos[-1][1]-paddle_pos>-8:
-            paddle_vel=0
-        elif ball_pos[-1][1]-paddle_pos<-8:
-            paddle_vel=-move_unit*2
-    else:
-        paddle_vel=0
+    ballarray=np.array(ball_pos[-1])[:, np.newaxis]
+    padarray=np.array(paddle_pos)
+    
+    x_input=np.vstack([ballarray, padarray]).T
+    paddle_vel=int(load_model.predict(x_input)[0])
+    print("x_input,paddle_vel:",x_input,paddle_vel)
